8bf846c1dcca4216456b79256b51d5c697936e1d
David <sobekcore@gmail.com>
2022-04-30 22:13:20 +0200

    Initial package setup

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const selects = document.querySelectorAll('select[multiple]');
+
+  for (const select of selects) {
+    // Build custom select element on top of each of this elements
+    // Custom select must be simple, yet fully accessible and functional
+  }
+});
