5ed5e3c7219340cb6a5f60fc2f6d4001c1a02ab1
David <sobekcore@gmail.com>
2022-05-01 03:08:18 +0200

    Better removing of selected options

--- src/index.js
+++ src/index.js
@@ -1,62 +1,67 @@
 import './index.css';
 
-if (typeof document !== 'undefined') {
-  document.addEventListener('DOMContentLoaded', () => {
-    const selects = document.querySelectorAll('select[multiple]');
+const TAG_REMOVE_WIDTH = 16;
 
-    for (const select of selects) {
-      select.classList.add('select-multiple-hidden');
+document.addEventListener('DOMContentLoaded', () => {
+  const selects = document.querySelectorAll('select[multiple]');
 
-      const wrapper = document.createElement('div');
-      wrapper.classList.add('select-multiple');
-      select.after(wrapper);
+  for (const select of selects) {
+    select.classList.add('select-multiple-hidden');
 
-      const input = document.createElement('div');
-      input.setAttribute('role', 'input');
-      input.setAttribute('tabindex', '0');
-      wrapper.append(input);
+    const options = select.querySelectorAll('option');
 
-      const list = document.createElement('ul');
-      list.setAttribute('role', 'listbox');
-      wrapper.append(list);
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('select-multiple');
+    select.after(wrapper);
 
-      const options = select.querySelectorAll('option');
+    const input = document.createElement('div');
+    input.setAttribute('role', 'input');
+    input.setAttribute('tabindex', '0');
+    wrapper.append(input);
 
-      for (const option of options) {
-        const item = document.createElement('li');
-        item.setAttribute('data-value', option.value);
-        item.setAttribute('role', 'option');
-        item.innerText = option.innerText;
-        list.append(item);
+    const list = document.createElement('ul');
+    list.setAttribute('role', 'listbox');
+    wrapper.append(list);
 
-        item.addEventListener('click', () => {
+    for (const option of options) {
+      const item = document.createElement('li');
+      item.setAttribute('data-value', option.value);
+      item.setAttribute('role', 'option');
+      item.innerText = option.innerText;
+      list.append(item);
+
+      item.addEventListener('click', () => {
+        item.classList.toggle('hidden');
+        option.setAttribute('selected', 'selected');
+
+        const tag = document.createElement('tag');
+        tag.innerText = item.innerText;
+
+        const remove = document.createElement('i');
+        remove.innerHTML = '&times;';
+
+        tag.append(remove);
+        input.append(tag);
+
+        remove.addEventListener('click', () => {
           item.classList.toggle('hidden');
-          option.setAttribute('selected', 'selected');
-
-          const tag = document.createElement('tag');
-          tag.innerText = item.innerText;
-          input.append(tag);
-
-          tag.addEventListener('click', () => {
-            item.classList.toggle('hidden');
-            option.removeAttribute('selected');
-            tag.remove();
-          }, { once: true });
-        });
-      }
-
-      const { width } = list.getBoundingClientRect();
-      input.style.width = `${width}px`;
-
-      wrapper.addEventListener('click', () => {
-        list.classList.add('visible');
-      });
-
-      document.addEventListener('click', (event) => {
-        if (!wrapper.contains(event.target)) {
-          list.classList.remove('visible');
-        }
+          option.removeAttribute('selected');
+          tag.remove();
+        }, { once: true });
       });
     }
-  });
-}
+
+    const { width } = list.getBoundingClientRect();
+    input.style.width = `${width + TAG_REMOVE_WIDTH}px`;
+
+    wrapper.addEventListener('click', () => {
+      list.classList.add('visible');
+    });
+
+    document.addEventListener('click', (event) => {
+      if (!wrapper.contains(event.target)) {
+        list.classList.remove('visible');
+      }
+    });
+  }
+});
